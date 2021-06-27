eeb9b30068696559d849f1305325433162a1aa0e
Aashiish-Pandey <ash006pandey@gmail.com>
2021-06-27 10:53:39 +0530

    Button , ButtonGroup, Container component

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,18 +1,37 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Container from "@material-ui/core/Container";
 
 export default function Create() {
   return (
-    <div>
-      <Typography 
-      variant ='h6'
-      component='h2'
-      color ='textSecondary'
-      gutterBottom
+    <Container>
+      <Typography
+        variant="h6"
+        component="h2"
+        color="textSecondary"
+        gutterBottom
       >
-         create a new note 
+        create a new note
       </Typography>
-      
-    </div>
+
+      <Button type ="submit" 
+      variant="contained" 
+      color="secondary"
+      onClick ={()=>console.log('clicked')}
+      >Submit</Button>
+
+      {/* <Button type="submit">Submit</Button>
+      <Button type="submit" color="secondary" variant="outlined">
+        Submit
+      </Button>
+      <ButtonGroup color ="secondary" variant="contained">
+        <Button>one</Button>
+        <Button>one</Button>
+        <Button>one</Button>
+        <Button>one</Button>
+      </ButtonGroup> */}
+    </Container>
   );
 }
