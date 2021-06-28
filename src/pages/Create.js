1aac013cfef51f10a4a03d4ed19d652c021a7beb
Aashiish-Pandey <ash006pandey@gmail.com>
2021-06-28 09:51:41 +0530

    Icons component

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -3,6 +3,9 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Container from "@material-ui/core/Container";
+import AcUnitIcon from "@material-ui/icons/AcUnit";
+import SendIcon from "@material-ui/icons/Send";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 
 export default function Create() {
   return (
@@ -16,11 +19,25 @@ export default function Create() {
         create a new note
       </Typography>
 
-      <Button type ="submit" 
-      variant="contained" 
-      color="secondary"
-      onClick ={()=>console.log('clicked')}
-      >Submit</Button>
+      <Button
+        type="submit"
+        variant="contained"
+        color="secondary"
+        onClick={() => console.log("clicked")}
+        startIcon={<SendIcon />}
+        endIcon={<KeyboardArrowRightIcon />}
+      >
+        Submit
+      </Button>
+      <br />
+
+      {/* icons */}
+      <AcUnitIcon />
+      <AcUnitIcon color="secondary" fontSize="large" />
+      <AcUnitIcon color="secondary" fontSize="small" />
+      <AcUnitIcon color="action" fontSize="small" />
+      <AcUnitIcon color="error" fontSize="small" />
+      <AcUnitIcon color="disabled" fontSize="small" />
 
       {/* <Button type="submit">Submit</Button>
       <Button type="submit" color="secondary" variant="outlined">
